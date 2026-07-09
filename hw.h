f56902ec81f44c2b9287eb441f57815a047c5838
Niz Ansari <nizansari@macbookair.home>
2026-07-09 18:43:52 -0400

    Created files for hardware implementation

---
+++ hw.h
@@ -0,0 +1,9 @@
+#ifndef HW_H
+#define HW_H
+
+void hw_init(void);
+void hw_cleanup(void);
+void hw_set_freq(double f_hz);
+void hw_read_channels(int N, double fs, double f_hz, double *ch_ref, double *ch_dut);
+
+#endif

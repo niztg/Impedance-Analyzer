5931cc638a6b2b864e0f000cb338e64642c719c5
Niz Ansari <nizansari@macbookair.home>
2026-07-10 01:21:48 -0400

    Finish hw.c file, add mcp3202 read function and init function

--- hw.c
+++ hw.c
@@ -13,6 +13,10 @@
 #include <stdlib.h>
 #include <time.h>
 
+#ifndef TIMER_ABSTIME
+#define TIMER_ABSTIME 1
+#endif
+
 #ifdef SIM_MODE
 
 #define SIM_R 470.0 // series resistance of DUT
@@ -70,7 +74,7 @@ static void spi_delay(void){
 }
 
 /*
-AD9833 functions
+AD9833 (DAC) functions
 */
 
 // Template to write a value into the AD9833
@@ -102,3 +106,77 @@ void hw_set_freq(double f_hz){
     nanosleep(&ts, NULL);
 }
 
+/*
+MCP3202 (ADC) functions
+*/
+static uint16_t mcp3202_read(int channel){
+    uint16_t result = 0;
+    int odd = channel & 1;
+
+    gpio_set(PIN_CS_ADC, 0); // enable
+    spi_delay();
+
+    gpio_set(PIN_MOSI, 1); // start bit, initiates conversion                              
+    gpio_set(PIN_SCLK, 1); spi_delay();
+    gpio_set(PIN_SCLK, 0); spi_delay();
+
+    gpio_set(PIN_MOSI, 1); // SGL = 1, asserts single-channel mode                            
+    gpio_set(PIN_SCLK, 1); spi_delay();
+    gpio_set(PIN_SCLK, 0); spi_delay();
+
+    gpio_set(PIN_MOSI, odd); // channel select                         
+    gpio_set(PIN_SCLK, 1); spi_delay();
+    gpio_set(PIN_SCLK, 0); spi_delay();
+
+    gpio_set(PIN_MOSI, 1); // MSBF = 1, "most significant bit first", arbitrary                                
+    gpio_set(PIN_SCLK, 1); spi_delay();
+    gpio_set(PIN_SCLK, 0); spi_delay();
+
+    for (int i = 11; i >= 0; i--) {
+        gpio_set(PIN_SCLK, 1); spi_delay(); // clocks to put the value of the i-th bit on the MISO pin
+        result |= (uint16_t)(gpio_get(PIN_MISO) << i); // stores the value in the i-th bit of the local result variable
+        gpio_set(PIN_SCLK, 0); spi_delay();
+    }
+
+    gpio_set(PIN_CS_ADC, 1);
+    return result;
+}
+
+void hw_read_channels(int N, double fs, double f_hz, double* ch_ref, double* ch_dut){
+    (void) f_hz; // don't use this variable, the function has to have the same function signature as the sim version
+    long period_ns = (long) 1e9 / fs;
+    struct timespec next;
+    clock_gettime(CLOCK_MONOTONIC, &next);
+
+    for (int n = 0; n < N; n++){
+        // the MCP3202 returns a 12 bit number up to 4095
+        // 4096.0 normalizes this to a number between 0 and 1
+        // because we always take the ratio of the two channels, the  value normalization factor is irrelevant
+        ch_ref[n] = (double)mcp3202_read(0) / 4096.0;
+        ch_dut[n] = (double)mcp3202_read(1) / 4096.0;
+
+        next.tv_nsec += period_ns;
+        if (next.tv_nsec >= 1000000000L){
+            next.tv_nsec -= 1000000000L;
+            next.tv_nsec += 1;
+        }
+        clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next, NULL);
+    }
+}
+
+void hw_init(void){
+    // Set all SPI pins as outputs except MISO
+    *jp1_dir_ptr |=  (1 << PIN_SCLK) | (1 << PIN_MOSI) |
+                     (1 << PIN_CS_DAC) | (1 << PIN_CS_ADC);
+    *jp1_dir_ptr &= ~(1 << PIN_MISO);
+
+    gpio_set(PIN_CS_DAC, 1);
+    gpio_set(PIN_CS_ADC, 1);
+    gpio_set(PIN_SCLK,   0);
+
+    ad9833_write_reg(0x2100);  // reset AD9833
+}
+
+void hw_cleanup(void) { /* nothing to close */ }
+
+#endif
